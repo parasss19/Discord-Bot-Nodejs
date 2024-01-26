5e234992dd18f2d9ee5ced2a67666253ed92b054
Paras <mehtaparas1901@gmail.com>
2024-01-26 17:07:53 +0530

    added bot to server

---
+++ app.js
@@ -0,0 +1,27 @@
+const {Client, GatewayIntentBits}  = require ('discord.js');
+
+const client = new Client( { 
+    intents : [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+//created listener on client using request = messageCreate
+client.on("messageCreate", (message)=> {
+    // console.log(message)          //it contain so many properties and info about user
+    // console.log(message.content)  // it show the content or basically the msg user type
+
+    if(message.author.bot){
+        return;               //here if condition is used to prevent the bot to reply to its own msg
+    }else{
+      message.reply({
+        content : "Hi from Bot1119",
+      })
+    }
+});
+
+//Login using the token
+client.login("MTIwMDM2NDAyNzczNDM5NzAxOA.GUos4s.Xew0DsWaui4mzJmufQA0b1JeXaRslcKqTyICWY")
+
